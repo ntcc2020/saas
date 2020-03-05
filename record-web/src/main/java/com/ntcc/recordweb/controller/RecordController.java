ad84aa7e6e84e316086ce43e0f7320dfeb8d4237
goofeng <43925690+goofeng@users.noreply.github.com>
2020-03-05 15:20:55 +0800

    record demo

--- record-web/src/main/java/com/ntcc/recordweb/controller/RecordController.java
+++ record-web/src/main/java/com/ntcc/recordweb/controller/RecordController.java
@@ -1,8 +1,11 @@
 package com.ntcc.recordweb.controller;
 
 import com.ntcc.recordweb.service.RecordWebService;
+import com.ntcc.saascommon.model.ModelJsonResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,4 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class RecordController {
     @Autowired
     private RecordWebService recordWebService;
+
+    @RequestMapping(value = "/countToday", method = RequestMethod.GET)
+    public String countToday() {
+        log.info("RecordController countToday ");
+
+        ModelJsonResult returnValue = new ModelJsonResult();
+        //returnValue.setResult(recordWebService.register(phoneNum));
+
+        return returnValue.toJson();
+    }
 }
