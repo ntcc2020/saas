650a3fd80941aa1cd16fa2c32a4d64d58392cc4e
goofeng <43925690+goofeng@users.noreply.github.com>
2020-03-06 11:22:28 +0800

    salary

--- salary-web/src/main/java/com/ntcc/salaryweb/controller/SalaryController.java
+++ salary-web/src/main/java/com/ntcc/salaryweb/controller/SalaryController.java
@@ -1,8 +1,12 @@
 package com.ntcc.salaryweb.controller;
 
+import com.ntcc.saascommon.model.ModelJsonResult;
 import com.ntcc.salaryweb.service.SalaryWebService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,4 +22,42 @@ import org.springframework.web.bind.annotation.RestController;
 public class SalaryController {
     @Autowired
     private SalaryWebService salaryWebService;
+
+    /*
+     *  日工资统计
+     */
+    @RequestMapping(value = "/countByDay", method = RequestMethod.GET)
+    public String countByDay(@RequestParam(name = "companyId",required = true) Integer companyId) {
+        log.info("RecordController countToday ");
+
+        ModelJsonResult returnValue = new ModelJsonResult();
+        //returnValue.setResult(recordWebService.register(phoneNum));
+
+        return returnValue.toJson();
+    }
+
+    /*
+     * 工资明细
+     */
+    @RequestMapping(value = "/detail", method = RequestMethod.GET)
+    public String detail(@RequestParam(name = "companyId",required = false) Integer companyId) {
+        log.info("RecordController countToday " + companyId);
+
+        ModelJsonResult returnValue = new ModelJsonResult();
+
+        return returnValue.toJson();
+    }
+
+
+    /*
+     * 本月工资统计，用于日历
+     */
+    @RequestMapping(value = "/countByMonth", method = RequestMethod.GET)
+    public String countByMonth(@RequestParam(name = "companyId",required = false) Integer companyId) {
+        log.info("RecordController countToday " + companyId);
+
+        ModelJsonResult returnValue = new ModelJsonResult();
+
+        return returnValue.toJson();
+    }
 }
